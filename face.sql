b1a04534dc4b4239673a361a27d4213c3445c875
Kauã <47856908+kauarijoaguiar@users.noreply.github.com>
2021-07-12 12:27:14 -0300

    verificar o face

--- face.sql
+++ face.sql
@@ -36,77 +36,71 @@ e arrumei as ligações
     );
 
     create table usuario(
-    codigo integer not null,
+    email char (100) not null,
     nome char(100) not null,
-    dataa datetime,
-    cidade char(100) not null,
-    pais char(100) not null,
-    estado char(100) not null,
-    primary key (codigo)
+    primary key (email)
     );
 
-    insert into usuario(codigo, nome, dataa, cidade, pais, estado) values
-    (1,'Professor de BD', '2010-01-01 09:00','Rio Grande', 'Brasil','RS'), 
-    (2, 'João Silva Brasi', '2020-01-01 13:00','Rio Grande', 'Brasil','RS'), 
-    (3,'Pedro Alencar Pereira','2020-01-01 13:05','Rio Grande', 'Brasil','RS'), 
-    (4, 'Maria Cruz Albuquerque','2020-01-01 13:10','Rio Grande', 'Brasil','RS'), 
-    (5, 'Joana Rosa Medeiros','2020-01-01 13:15','Rio Grande', 'Brasil','RS'), 
-    (6, 'Paulo Xavier Ramos','2020-01-01 13:20','Rio Grande', 'Brasil','RS'),
-    (7, 'IFRS campus Rio Grande' ,null,'Rio Grande', 'Brasil','RS');
+    insert into usuario(email, nome) values
+    ('joaosbras@mymail.com', 'João Silva Brasi'), 
+    ('mcalbuq@mymail.com', 'Maria Cruz Albuquerque'), 
+    ('jorosamed@mymail.com', 'Joana Rosa Medeiros'), 
+    ('pxramos@mymail.com', 'Paulo Xavier Ramos'),
+    ('pmartinssilva90@mymail.com', 'Paulo Martins Silva'),
+    ('pele@cbf.com.br', 'Edson Arantes do Nascimento');
 
     create table amigoscomum_usuario(
-        cod_usuario integer not null,
+        email_usuario char (100) not null,
         cod_amigoscomum integer not null,
-        foreign key (cod_usuario) references usuario(codigo),
+        foreign key (email_usuario) references usuario(email),
     foreign key (cod_amigoscomum) references amigoscomum(codigo),
-    primary key (cod_usuario, cod_amigoscomum)
+    primary key (email_usuario, cod_amigoscomum)
     );
 
     create table amigoscomum(
     codigo integer not null,
     cod_amigos integer not null,
-    cod_usuario integer not null,
+    email_usuario char (100) not null,
     foreign key (cod_amigos) references amigos(codigo),
-    foreign key (cod_usuario) references usuario(codigo),
+    foreign key (email_usuario) references usuario(email),
     primary key (codigo)
     );
 
     create table amigos(
     codigo integer not null,
-    cod_usuario integer not null,
+    email_usuario char (100) not null,
     nome char (100) not null,
     dataa datetime,
     pais char(100) not null,
-    foreign key (cod_usuario) references usuario(codigo),
+    foreign key (email_usuario) references usuario(email),
     primary key (codigo)
     );
 
-    insert into amigos(codigo,cod_usuario, nome, dataa, pais) values
+/*
+    insert into amigos(codigo,email_usuario, nome, dataa, pais) values
     (1,1,'João Silva Brasil', '2021-05-17 10:00', 'Brasil'),
     (2,1,'Pedro Alencar Pereira', '2021-05-17 10:05', 'Brasil' ),
     (3,1,'Maria Cruz Albuquerque','2021-05-17 10:10','Brasil' ),
     (4,1,'Joana Rosa Medeiros','2021-05-17 10:15','Brasil' ),
     (5,1,'Paulo Xavier Ramos','2021-05-17 10:20','Brasil' );
-    
+*/ 
 
     create table post(
     codigo integer not null,
-    cod_usuario integer not null,
+    email_usuario char (100) not null,
     post char(1000) not null,
     cidade char (100) not null, 
     estado char (100) not null,
     pais char (100) not null,
     dataa datetime, 
-    foreign key (cod_usuario) references usuario(codigo),
+    foreign key (email_usuario) references usuario(email),
     primary key (codigo)
     );
 
-    insert into post(codigo, cod_usuario, post, cidade, estado, pais, dataa )values
-    (1,1,'Hoje eu aprendi como inserir dados no SQLite no IFRS','Rio Grande', 'RS', 'Brasil','2021-06-02 15:00'),
-    (2,1,'Atendimento de BD no GMeet amanhã para quem tiver dúvidas de INSERT', 'Rio Grande', 'RS', 'Brasil', '2021-06-02 15:35'),
-    (3,5,'Alguém mais ficou com dúvida no comando INSERT?', 'Rio Grande', 'RS', 'Brasil','2021-06-02 15:15'),
-    (4,6,'Eu também', 'Rio Grande', 'RS', 'Brasil','2021-06-02 15:20'),
-    (5,2, 'Já agendaste horário de atendimento com o professor?', 'Rio Grande', 'RS', 'Brasil', '2021-06-02 15:30');
+    insert into post(codigo, email_usuario, post, cidade, estado, pais, dataa )values
+    (3,'jorosamed@mymail.com','Alguém mais ficou com dúvida no comando INSERT?', 'Rio Grande', 'RS', 'Brasil','2021-06-02 15:15'),
+    (4,'pxramos@mymail.com','Eu também', 'Rio Grande', 'RS', 'Brasil','2021-06-02 15:20'),
+    (5,'joaosbras@mymail.com', 'Já agendaste horário de atendimento com o professor?', 'Rio Grande', 'RS', 'Brasil', '2021-06-02 15:30');
 
 
 create table assunto(
@@ -117,38 +111,34 @@ foreign key (cod_post) references post(codigo),
 primary key (codigo)
 );
 insert into assunto(codigo, cod_post, assunto) values
-(1, 1,'bd'),(2,1,'sqlite'),(3,2,'atendimento'),(4,2,'bd'),(5,2,'sqlite'),(6,2,'insert'),
 (7,3,'bd'), (8,3,'sqlite'), (9,3,'insert'),(10,5,'atendimento'), (11,5,'bd');
 
 create table citados(
     codigo integer not null,
     cod_post integer not null,
-    cod_usuario integer not null,
-foreign key (cod_usuario) references usuario(codigo),
+    email_usuario char (100) not null,
+foreign key (email_usuario) references usuario(email),
 foreign key (cod_post) references post(codigo),
 primary key (codigo)
 );
-insert into citados (codigo, cod_post, cod_usuario) values
-(1,1,7),(2,2,5),(3,2,6);
+insert into citados (codigo, cod_post, email_usuario) values
+(3,2,'pxramos@mymail.com');
 
     create table reacao(
     codigo integer not null, 
-    cod_usuario integer not null,
+    email_usuario char (100) not null,
     cod_post integer not null,  
     cidade char(100) not null, 
     estado char(2) not null, 
     pais char(100) not null, 
     dataa datetime,
-    foreign key (cod_usuario) references usuario(codigo),
+    foreign key (email_usuario) references usuario(email),
     foreign key (cod_post) references post(codigo),
     primary key (codigo)
     );
 
-    insert into reacao(codigo, cod_usuario, cidade, estado, pais, cod_post, dataa)values
-    (1,3,'Rio Grande', 'RS', 'Brasil',1, '2021-06-02 15:05'),
-    (2,4,'Rio Grande', 'RS', 'Brasil',1,'2021-06-05 15:10'),
-    (3,4,'Rio Grande', 'RS', 'Brasil',1,'2021-06-05 15:10'),
-    (4,6,'Rio Grande', 'RS', 'Brasil',3, '2021-06-07 15:20' );
+    insert into reacao(codigo, email_usuario, cidade, estado, pais, cod_post, dataa)values
+    (4,'pxramos@mymail.com','Rio Grande', 'RS', 'Brasil',3, '2021-06-07 15:20' );
 
     create table comentario(
     codigo integer not null,
@@ -158,38 +148,38 @@ insert into citados (codigo, cod_post, cod_usuario) values
     );
 
     insert into comentario(codigo, cod_post, cod_post)values
-    (1,1,3),
     (2,3,4),
     (3,4,5);
 
     create table compartilhamento(
     codigo integer not null,
-    cod_usuario integer not null, 
+    email_usuario char (100) not null, 
     cod_post integer not null, 
     cidade char(100) not null, 
     estado char(2) not null, 
     dataa datetime,
-    foreign key (cod_usuario) references usuario(codigo),
+    foreign key (email_usuario) references usuario(codigo),
     foreign key (cod_post) references post(codigo),
     primary key (codigo)
     );
-
-    insert into compartilhamento(codigo, cod_usuario,cod_post, cidade, estado, dataa) values
+/*
+    insert into compartilhamento(codigo, email_usuario,cod_post, cidade, estado, dataa) values
     (1,2,2, 'Rio Grande', 'RS', '2021-06-02 15:40');
+*/
 
 create table usuariogrupo(
-    cod_usuario integer not null,
+    email_usuario integer not null,
     cod_grupo integer not null,
-    foreign key (cod_usuario) references usuario(codigo),
+    foreign key (email_usuario) references usuario(email),
     foreign key (cod_grupo) references grupo(codigo),
-    primary key (cod_usuario, cod_grupo)
+    primary key (email_usuario, cod_grupo)
 );
 
 create table grupo(
     codigo integer not null,
-    cod_usuario integer not null,
+    email_usuario integer not null,
     nome char(100) not null,
-    foreign key (cod_usuario) references usuario(codigo),
+    foreign key (email_usuario) references usuario(email),
     primary key (codigo)
 );
 
